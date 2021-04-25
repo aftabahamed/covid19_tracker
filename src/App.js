d20b1fd707b4d163b06e5488715205e7cdc91076
Aftab <aftabahamedvm@gmail.com>
2021-04-25 20:40:23 +0530

    covid-19 tracker with countries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from  './api'
 
 class App extends React.Component {
+
+    state = {
+        data: {},
+        country: ''
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
+    }
+
     render() {
+        const { data, country } = this.state;
         return(
-            <div>
-                <h1>Basic folder structure</h1>
+            <div className={styles.container}>
+                <img className={styles.image} src='https://i.ibb.co/7QpKsCX/image.png' alt="COVID-19" />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
             </div>
         )
     }
